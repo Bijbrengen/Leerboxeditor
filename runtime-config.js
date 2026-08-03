9c14b4edadb5a6b8ce9ea87ee0551f276affd3a4
Bijbrengen <slagter@bijbrengen.nl>
2026-08-03 08:37:08 +0200

    Zorgen dat standalone Phile goed communiceert met Engine, Dashboard en Editor

--- runtime-config.js
+++ runtime-config.js
@@ -1,14 +1,6 @@
-(function() {
-  var isLocal = typeof window !== "undefined" && (
-    window.location.hostname === "127.0.0.1" ||
-    window.location.hostname === "localhost"
-  );
-  var tunnelUrl = "https://intent-carries-travelers-media.trycloudflare.com/api";
-
-  window.LEERBOX_EDITOR_CONFIG = Object.freeze({
-    "apiBase": isLocal ? "http://127.0.0.1:47111/api" : tunnelUrl,
-    "editorUrl": isLocal ? "http://127.0.0.1:47114/" : "https://bijbrengen.github.io/LeerboxEditor/",
-    "dashboardUrl": isLocal ? "http://127.0.0.1:47112/" : "https://bijbrengen.github.io/LeerpretDashboard/",
-    "learngameOmUrl": isLocal ? "http://127.0.0.1:47113/" : "https://bijbrengen.github.io/Learngame-Operations-Management/"
-  });
-})();
+window.LEERBOX_EDITOR_CONFIG = Object.freeze({
+  "apiBase": "http://127.0.0.1:47111/api",
+  "editorUrl": "http://127.0.0.1:47114/",
+  "dashboardUrl": "http://127.0.0.1:47112/",
+  "learngameOmUrl": "http://127.0.0.1:47113/"
+});
